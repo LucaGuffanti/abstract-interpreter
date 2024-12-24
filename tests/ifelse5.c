cf614bc75384d404b36404079eb4080eca99473d
Luca Guffanti <luca2.guffanti@mail.polimi.it>
2024-12-24 11:45:34 +0000

    add if else tests

--- tests/ifelse5.c
+++ tests/ifelse5.c
@@ -6,15 +6,18 @@ void main() {
   /*!npk b between 0 and 2 */
   if(b == 0) {
     a = 1;
+    b = a * 3;
   }
   else {
     if(b == 1) {
       a = 2;
     }
     else {
-
+      a = 6;
     }
   }
-  assert(a <= 10);
+  assert(a <= 6);
   assert(a >= 0);
+  assert(b >= 1);
+  assert(b <= 3);
 }
